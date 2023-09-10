93933073ed540fa678d0d3f8acfcb4d5f6531940
Altriko Wicaksono <altrikowicaksono@gmail.com>
2023-09-11 00:12:02 +1000

    Delete the CSV files

--- Main.py
+++ Main.py
@@ -5,5 +5,6 @@ start = '2020-01-01'  # (3)
 end = None  # (4)
 df = yfinance.download(tic, start, end, ignore_tz=True)  # (5)
 print(df)  # (6)
-df.to_csv('qan_stk_prc.csv')  # (7)
+# df.to_csv('qan_stk_prc.csv')  # (7)
 
+print("Hello World")
